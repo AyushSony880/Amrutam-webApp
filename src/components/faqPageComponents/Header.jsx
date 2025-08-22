f87dc00dbb8e9ba2fd85511c79f004941e81d7ca
Ayush <ayushak880@gmail.com>
2025-08-22 22:43:35 +0530

    website completed fully responsive

--- src/components/faqPageComponents/Header.jsx
+++ src/components/faqPageComponents/Header.jsx
@@ -2,7 +2,6 @@ import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import { FiPhone } from "react-icons/fi";
 import { FaChevronDown } from "react-icons/fa";
-import navIcon1 from "../../assets/faqPageAssets/navIcons/navIcon1.svg";
 import navIcon2 from "../../assets/faqPageAssets/navIcons/navIcon2.svg";
 import navIcon3 from "../../assets/faqPageAssets/navIcons/navIcon3.svg";
 import navIcon4 from "../../assets/faqPageAssets/navIcons/navIcon4.svg";
@@ -19,30 +18,30 @@ const Header = () => {
         <FiPhone className="inline" /> 9826352321
       </p>
       <img className=" h-8 sm:h-[37px] " src={logo} alt="logo" />
-      <div className="  flex flex-col lg:flex-row items-center gap-x-5 sm:px-5 justify-start lg:justify-end w-full ">
+      <div className="  flex flex-col lg:flex-row items-center gap-x-5 sm:px-5 justify-center lg:justify-center w-full ">
         <ul className="  flex-wrap  flex items-center justify-around  text-nowrap  gap-x-2.5 sm:gap-x-10 text-xs sm:text-sm md:text-[20px] font-medium sm:font-semibold  md:font-bold text-[#3A643B] ">
           <li>
             <NavLink to="/">Home</NavLink>
           </li>
           <li>
-            <NavLink to="/find-doctors">Find Doctors</NavLink>
+            <NavLink to="/">Find Doctors</NavLink>
           </li>
           <li>
-            <NavLink to="/lab-tests">Lab Tests</NavLink>
+            <NavLink to="/">Lab Tests</NavLink>
           </li>
           <li>
-            <NavLink to="/shop">Shop</NavLink>
+            <NavLink to="/">Shop</NavLink>
           </li>
           <li>
-            <NavLink to="/forum">Forum</NavLink>
+            <NavLink to="/">Forum</NavLink>
           </li>
           <li>
-            <NavLink to="/forum">About Us</NavLink>
+            <NavLink to="/">About Us</NavLink>
           </li>
         </ul>
         <div className="flex self-end flex-wrap my-2 items-center justify-center gap-1  md:gap-2">
           {allIcons.map((link, i) => (
-            <Link key={i} target="_blank">
+            <Link key={i}>
               <img
                 className="w-7 h-7 sm:w-8 sm:h-8  md:w-9 md:h-9 lg:w-10 lg:h-10 object-center"
                 src={link}
